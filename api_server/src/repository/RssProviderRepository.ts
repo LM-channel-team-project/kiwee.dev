054c69e1c0fc75d9a1e22315b8cdc0bfc95c0691
천정호 <cjsgh85@gmail.com>
2021-05-01 19:01:14 +0900

    [FIXED] RssProvider 스키마 수정

--- api_server/src/repository/RssProviderRepository.ts
+++ api_server/src/repository/RssProviderRepository.ts
@@ -1,12 +1,10 @@
 import { v4 as uuidV4 } from 'uuid';
 import RssProviderModel from '../model/RssProvider';
-import { providerType, RssProviderType } from '../type/RssProviderType';
 
-export interface SaveRssProviderProps {
-  providerName: string;
+export interface SaveProviderProps {
   providerEmail: string;
+  providerName: string;
   providerAvatar?: string;
-  providerType: providerType;
   rssLink: String;
 }
 
@@ -17,18 +15,11 @@ const RssProviderRepository = class {
     providerEmail,
     providerAvatar,
     providerName,
-    providerType,
     rssLink,
-  }: SaveRssProviderProps): Promise<{
+  }: SaveProviderProps): Promise<{
     code: number;
     message: string;
   }> => {
-    const isExist = await this.rssProviderModel.exists({ providerEmail });
-    if (isExist)
-      return {
-        code: 400,
-        message: '이미 존재하는 이메일입니다.',
-      };
     try {
       const providerId = uuidV4();
       const lastModifiedTime = new Date();
@@ -37,20 +28,19 @@ const RssProviderRepository = class {
         providerEmail,
         providerName,
         providerAvatar,
-        providerType,
         rssLink,
         lastModifiedTime,
         numOfArticles: 0,
       });
       await this.rssProviderModel.create(rssProvider);
       return {
-        code: 200,
+        code: 201,
         message: '성공적으로 등록되었습니다.',
       };
     } catch (e) {
       console.log(e.message);
       return {
-        code: 503,
+        code: 500,
         message: '오류가 발생했습니다.',
       };
     }
--- rss_batch_crawler/src/type/RssProviderType.ts
+++ rss_batch_crawler/src/type/RssProviderType.ts
@@ -1,15 +1,8 @@
-export type providerType = 'Medium' | 'TStory' | 'Velog' | 'Other';
-
-export interface RssProviderRequest {
-  
-}
 export interface RssProviderType {
   providerId: string;
   providerName: string;
   providerEmail: string;
   providerAvatar: string;
-  providerType: providerType;
   rssLink: String;
   lastModifiedTime: Date;
-  numOfArticles: number;
 }
