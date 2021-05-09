141a2430da2b0525c325969b7c115aae0d983e91
97baek <170410_@naver.com>
2021-05-09 17:50:27 +0900

    [FIX] 타입 에러 수정

--- front/src/hooks/useAxios.ts
+++ front/src/hooks/useAxios.ts
@@ -1,22 +1,26 @@
 import axios from 'axios';
+import { IArticle } from '@/types/article';
 import { useState, useEffect } from 'react';
 
 const axiosArticles = async (url: string) => {
   const res = await axios(url);
   const data = await res.data;
+  console.log(data);
   return data;
 };
 
-export const useAxios = (url: string): string[] => {
-  const [articles, setArticles] = useState([]);
+export const useAxios = (url: string) => {
+  const [articles, setArticles] = useState<IArticle[]>([]);
 
   useEffect(() => {
     const getArticles = async () => {
       const data = await axiosArticles(url);
-      setArticles(data);
+      setArticles(data.data);
+      console.log(articles);
+      console.log(data);
     };
     getArticles();
-  }, [url]);
+  }, []);
 
   return articles;
 };
