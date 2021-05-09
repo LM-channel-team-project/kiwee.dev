315d5426666132d9b46333f231113c70887f1feb
97baek <170410_@naver.com>
2021-05-09 17:08:28 +0900

    [ADD] Article 타입 추가

---
+++ front/src/types/article.d.ts
@@ -0,0 +1,12 @@
+export interface IArticle {
+  articleId: string;
+  articleUrl: string;
+  insertDate: string;
+  keywords: string[];
+  providerAvatar: string;
+  providerId: string;
+  providerName: string;
+  thumbnail: string;
+  title: string;
+  id: string;
+}
