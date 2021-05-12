9313b9f1469eb3599ab6fa524230525ce2159800
97baek <170410_@naver.com>
2021-05-12 17:39:51 +0900

    [FIX] 불필요한 import 제거

--- front/src/components/Settings/ToggleNewTab/ToggleNewTab.tsx
+++ front/src/components/Settings/ToggleNewTab/ToggleNewTab.tsx
@@ -1,5 +1,5 @@
 import { useNewTabContext } from '@/hooks/useNewTabContext';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SwitchButton, SwitchOnOff } from '../common/styles';
 import { Container } from '../ToggleDarkmode/styles';
 
