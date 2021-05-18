4a85b38560aae838badf639353623888087a2d9b
천정호 <cjsgh85@gmail.com>
2021-05-18 19:32:44 +0900

    [ADD] Comments 모델 추가

---
+++ api_server/src/type/CommentsType.ts
@@ -0,0 +1,10 @@
+export interface Comment {
+  providerId: string;
+  content: string;
+  insertedDate: Date;
+  lastUpdatedDate: Date;
+}
+export default interface CommentsType {
+  articleId: string;
+  comments: Comment[];
+}
