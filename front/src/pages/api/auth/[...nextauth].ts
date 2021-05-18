47e432cac7992fa6bee4a9f1454873c2b9742523
천정호 <cjsgh85@gmail.com>
2021-05-18 16:50:50 +0900

    [UPDATE] github 로그인시 이메일 이슈 해결
    - github accessToken을 이용하여 추가적으로 email api를 호출.
    - Oauth2.0 로그인 이후 반환 받은 정보를 통해 서비스 회원 정보 초기화.
    - JWT Encryption 옵션 추가
    - JWT의 sub값을 통해 providerId를 저장하여 클라이언트에서 접근할 수 있도록함.

--- front/src/pages/api/auth/[...nextauth].ts
+++ front/src/pages/api/auth/[...nextauth].ts
@@ -1,9 +1,12 @@
+import axios from 'axios';
 import NextAuth, { Account, NextAuthOptions, Profile, Session, User } from 'next-auth';
 import { Awaitable, NextApiRequest, NextApiResponse } from 'next-auth/internals/utils';
 import { JWT } from 'next-auth/jwt';
 import Providers from 'next-auth/providers';
 
 const JWT_SECRET = process.env.JWT_SECRET;
+const GITHUB_EMAIL_API = 'https://api.github.com/user/emails';
+const API_SERVER_URL = process.env.API_SERVER_URL;
 
 const jsonify = (obj: any) => JSON.stringify(obj, null, 2);
 const options: NextAuthOptions = {
@@ -19,29 +22,62 @@ const options: NextAuthOptions = {
   ],
   jwt: {
     secret: JWT_SECRET,
+    encryption: true,
   },
-  // database: process.env.DB_URL,
+  session: { jwt: true },
   callbacks: {
-    signIn(user: User, account: Account, profile: Profile): Awaitable<string | boolean> {
+    async signIn(user: User, account: Account, profile: Profile): Promise<string | boolean> {
       // TODO API 서버에 사용자 정보 저장 or 업데이트 요청
       console.log(`user: ${jsonify(user)}`);
       console.log(`account: ${jsonify(account)}`);
       console.log(`profile: ${jsonify(profile)}`);
+
+      // Oauth Login 이후 반환된 회원 정보 추가 및 업데이트 요청 (Upsert)
+      try {
+        const { provider, accessToken } = account;
+        const { id: providerId, image: avatar, name } = user;
+        let { email } = user;
+
+        // fetch email
+        if (provider === 'github') {
+          const response = await axios.get(GITHUB_EMAIL_API, {
+            headers: { Authorization: `Bearer ${accessToken}` },
+          });
+          const { email: responseEmail } = response.data[0];
+
+          if (!responseEmail) {
+            return false;
+          }
+          email = responseEmail;
+        }
+
+        const response = await axios.post(`${API_SERVER_URL}/provider`, {
+          providerId,
+          email,
+          avatar,
+          name,
+        });
+        console.log(`${API_SERVER_URL}/provider`, response.data);
+      } catch (e) {
+        console.log(`${API_SERVER_URL}/provider`, e.message);
+      }
+
       return true;
     },
-    session(session: Session, userOrToken: User | JWT): Awaitable<Session> { 
-      console.log(`session: ${jsonify(session)}`);
-      console.log(`userOrToken: ${jsonify(userOrToken)}`);
+    session(session: Session, userOrToken: User | JWT): Awaitable<Session> {
       session.sub = userOrToken.sub;
+      if (userOrToken.accessToken) {
+        session.accessToken = userOrToken.accessToken;
+      }
+
       return session;
     },
-    jwt(
-      token: JWT,
-    ): Awaitable<JWT> {
-      console.log(`token: ${jsonify(token)}`);
+    jwt(token, _, account): Awaitable<JWT> {
+      if (account?.accessToken) {
+        token.accessToken = account.accessToken;
+      }
       return token;
     },
   },
 };
-
 export default (req: NextApiRequest, res: NextApiResponse) => NextAuth(req, res, options);
