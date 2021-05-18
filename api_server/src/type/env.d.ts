9917a8bbc47bfdd96b338fdee214bee735bfa792
천정호 <cjsgh85@gmail.com>
2021-05-18 19:29:49 +0900

    [UPDATE] 환경 변수 인터페이스 선언

---
+++ api_server/src/type/env.d.ts
@@ -0,0 +1,12 @@
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    NODE_ENV: string;
+    PORT: number;
+    ATLAS_URI: string;
+    ATLAS_DATABASE: string;
+    ATLAS_USERNAME: string;
+    ATLAS_PASSWORD: string;
+    JWT_SECRET: string;
+    RESET_KEY: string;
+  }
+}
