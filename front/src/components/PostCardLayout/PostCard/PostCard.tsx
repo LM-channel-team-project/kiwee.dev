6b124e2cf58c36b1829cba5c539410afc6b97e7c
97baek <170410_@naver.com>
2021-05-21 14:45:46 +0900

    [FIX] 변수명 변경

--- front/src/components/PostCardLayout/PostCard/PostCard.tsx
+++ front/src/components/PostCardLayout/PostCard/PostCard.tsx
@@ -14,51 +14,55 @@ interface PropTypes {
 function PostCard({ data }: PropTypes) {
   const [isNewTab] = useNewTabContext();
   const [session] = useSession();
-  const [likes, setLikes] = useState(false);
+  const [canLike, setCanLike] = useState<boolean>(true);
   const providerId: string | unknown = session?.sub;
 
   // 처음 렌더링될 때 현재 접속자가 article에 좋아요를 눌렀는지 확인
   useEffect(() => {
     const likeUsers = data.likes;
     if (likeUsers.length === 0) {
-      setLikes(true);
+      setCanLike(true);
     } else {
       for (const id of likeUsers) {
         if (id.providerId === providerId) {
-          setLikes(false);
+          setCanLike(false);
         } else {
-          setLikes(true);
+          setCanLike(true);
         }
       }
     }
   }, []);
 
-  const postLikes = async (bool: boolean) => {
+  // 좋아요 요청
+  const requestLikes = async (bool: boolean) => {
     try {
       const response = await client.post('/api/likes', {
         articleId: data.articleId,
         isLike: bool,
       });
-      console.log('좋아요 성공', response);
+      console.log('좋아요 요청 성공', response);
     } catch (err) {
       console.log('좋아요 실패', err);
     }
   };
 
+  // 좋아요 버튼 클릭 시 호출
   const toggleLike = () => {
     const likeUsers = data.likes;
 
+    // 아무도 좋아요를 누르지 않은 게시글인 경우
     if (likeUsers.length === 0) {
-      postLikes(likes);
-      setLikes(!likes);
+      requestLikes(canLike);
+      setCanLike(!canLike);
     } else {
       for (const id of likeUsers) {
+        // likeUsers에 현재 접속중인 사용자의 id가 있는 경우 (이미 누른 경우)
         if (id.providerId === providerId) {
-          postLikes(likes);
-          setLikes(!likes);
+          requestLikes(canLike);
+          setCanLike(!canLike);
         } else {
-          postLikes(likes);
-          setLikes(!likes);
+          requestLikes(canLike);
+          setCanLike(!canLike);
         }
       }
     }
@@ -95,7 +99,7 @@ function PostCard({ data }: PropTypes) {
               <IconButton
                 iconName="like"
                 size="small"
-                styleType={likes ? 'primary' : 'default'}
+                styleType={canLike ? 'default' : 'primary'}
                 onClick={toggleLike}
               />
             </li>
