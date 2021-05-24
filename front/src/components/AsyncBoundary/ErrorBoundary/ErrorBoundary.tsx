7b0fd80d2dc2062aace139c7d48f86754a856cde
euntaek <zmn5119@gmail.com>
2021-05-24 23:44:37 +0900

    [ADD] ErrorBoundary 컴포넌트 추가
    리엑트 공식 문서에 나와있는 ErrorBoundary를 렌더링 하는 컴포넌트를 props로 받아 더 유연하게 처리할 수 있게 확장

---
+++ front/src/components/AsyncBoundary/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+
+type RenderFallbackProps<ErrorType extends Error = Error> = {
+  error: ErrorType;
+  reset: (...args: unknown[]) => void;
+};
+
+type RenderFallbackType = <ErrorType extends Error>(
+  props: RenderFallbackProps<ErrorType>,
+) => React.ReactNode;
+
+interface ErrorBoundaryProps {
+  renderFallback: RenderFallbackType;
+  resetKeys?: unknown[];
+}
+
+interface ErrorBoundaryState {
+  error: Error | null;
+}
+
+const initialState: ErrorBoundaryState = { error: null };
+
+class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = initialState;
+  }
+  resetErrorBoundary = () => {
+    this.setState(initialState);
+  };
+
+  static getDerivedStateFromError(error: Error) {
+    return { error, hasError: true };
+  }
+
+  componentDidUpdate(prevProps: ErrorBoundaryProps) {
+    if (this.state.error == null) return null;
+    if (isDifferentArray(prevProps.resetKeys, this.props.resetKeys)) {
+      this.resetErrorBoundary();
+    }
+  }
+
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+    console.log(error, errorInfo);
+  }
+
+  render() {
+    const { children, renderFallback } = this.props;
+    const { error } = this.state;
+
+    if (error !== null) {
+      return renderFallback({
+        error,
+        reset: this.resetErrorBoundary,
+      });
+    }
+    return children;
+  }
+}
+
+const isDifferentArray = (a: unknown[] = [], b: unknown[] = []) =>
+  a.length !== b.length || a.some((item, index) => !Object.is(item, b[index]));
+
+export default ErrorBoundary;
