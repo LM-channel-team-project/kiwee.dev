13a424ca3b804ec0a5c771fb6cc8404507c06892
euntaek <zmn5119@gmail.com>
2021-06-06 01:39:32 +0900

    [UPDATE] 좋아요 라우터 리팩토링

--- api_server/src/router/likesRouter.ts
+++ api_server/src/router/likesRouter.ts
@@ -1,49 +1,38 @@
 import { Request, Response, Router } from 'express';
 
-import { LikeModel } from '../model/Likes';
-
-import articleService from '../service/articleService';
+import likeService from '../service/likeService';
+import articleService from '../service/articleService.new';
+import LikeType from '../type/LikeType';
 
 const router = Router();
 
-// 특정 아티클의 Like를 누른 회원 조회
+// 특정 provider의 좋아요 조회
 router.get('/', async (req: Request, res: Response) => {
-  const articleId = req.query.articleId;
-  if (!articleId)
-    return res.status(401).json({ message: 'must send articleId' });
-
+  const providerId = req.query.providerId as string;
+  if (!providerId) return res.status(401).json({ message: 'providerId가 필요합니다.' });
   try {
-    const { likes } = (await articleService.findLikesByArticleId(
-      articleId as string
-    )) as LikeModel;
-    console.log(likes);
-    return res
-      .status(200)
-      .json({ message: '정상적으로 처리되었습니다.', likes });
+    const { likes } = (await likeService.findLikeByProviderId(providerId)) as LikeType;
+    if (!likes) return res.status(404).json({ message: '존재하지 않는 회원입니다.' });
+    return res.status(200).json({ message: '정상적으로 처리되었습니다.', likes });
   } catch (e) {
-    console.log(e.message);
-    return res.status(500).json({ message: '서버 에러' });
-  }
-});
-
-// like 버튼을 누른 경우 article의 numsOfLikes 증가 및 likes 배열에 추가
-router.post('/', async (req: Request, res: Response) => {
-  const { articleId, providerId, isLike } = req.body;
-  if (!articleId || !providerId || isLike === undefined)
-    return res
-      .status(401)
-      .json({ message: 'articleId, providerId, isLike가 필요합니다.' });
-
-  try {
-    const [updatedLikes, updatedArticle, updateProvider] =
-      await articleService.saveLike(articleId, providerId, isLike);
-    console.log('updatedLikes', updatedLikes);
-    console.log('updatedArticle', updatedArticle);
-    console.log('updateProvider', updateProvider);
-    return res.status(201).json({ message: '정상적으로 처리되었습니다.' });
-  } catch (e) {
-    console.log(e.message);
     return res.status(500).json({ message: e.message });
   }
 });
+
+// like 등록, 취소
+// isSave가 true인 경우 등록
+// isSave가 false인 경우 취소
+router.post('/', async (req: Request, res: Response) => {
+  const { articleId, providerId, isSave } = req.body;
+  if (!articleId || !providerId || isSave === undefined)
+    return res.status(401).json({ message: 'articleId, providerId, isSave가 필요합니다.' });
+  try {
+    const updateLikeResult = await articleService.updateOneByLike(providerId, articleId, isSave);
+    if (!updateLikeResult) throw new Error('아티클 라이크 업데이트 실패');
+    return res.status(201).json({ message: '정상적으로 처리되었습니다' });
+  } catch (e) {
+    return res.status(500).json({ message: e.message });
+  }
+});
+
 export default router;
