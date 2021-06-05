92d949d0ddf0199a83f7dab8ed864f19492c49a6
euntaek <zmn5119@gmail.com>
2021-06-06 01:46:51 +0900

    [UPDATE] 히스토리 레파지토리 리팩토링

--- api_server/src/repository/HistoryRepository.ts
+++ api_server/src/repository/HistoryRepository.ts
@@ -1,21 +1,36 @@
 import History from '../model/History';
+
 class HistoryRepository {
   private History = History;
   constructor() {}
   async createHistory(providerId: string) {
     const isExist = await this.History.exists({ providerId });
     if (isExist) return;
-    return this.History.create({ providerId, history: [] });
+    return await this.History.create({ providerId, history: [] });
   }
-  pushHistory(providerId: string, articleId: string) {
-    return this.History.findOneAndUpdate(
+  async insertHistory(providerId: string, articleId: string) {
+    return await this.History.findOneAndUpdate(
       { providerId },
       { $push: { histories: { articleId, insertedDate: new Date() } } },
-      { new: true }
+      { new: true },
     ).exec();
   }
-  findHistoryByProviderId(providerId: string) {
-    return this.History.findOne({ providerId }).exec();
+  async removeHistory(providerId: string, articleId: string) {
+    return await this.History.updateOne(
+      { providerId },
+      { $pull: { histories: { articleId } } },
+      { new: true },
+    ).exec();
+  }
+  async removeAllById(providerId: string) {
+    return await this.History.findOneAndUpdate(
+      { providerId },
+      { histories: [] },
+      { new: true },
+    ).exec();
+  }
+  async findHistoryByProviderId(providerId: string) {
+    return await this.History.findOne({ providerId }).exec();
   }
 }
 
