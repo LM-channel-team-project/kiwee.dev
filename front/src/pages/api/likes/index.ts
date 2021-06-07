bd37272bef5aa079816fb24620ba9e5a06174d3c
euntaek <zmn5119@gmail.com>
2021-06-08 00:14:35 +0900

    [UPDATE] likes API 수정

--- front/src/pages/api/likes/index.ts
+++ front/src/pages/api/likes/index.ts
@@ -1,12 +1,14 @@
-import { API_SERVER_URL } from '@/config/constants';
-import withCheckJwt from '@/wrapper/withCheckJwt';
 import axios from 'axios';
 import { NextApiRequest, NextApiResponse } from 'next-auth/internals/utils';
 import { JWT } from 'next-auth/jwt';
 
+import withCheckJwt from '@/wrapper/withCheckJwt';
+import { LikesResponse } from '@/types/response';
+import { API_SERVER_URL } from '@/config/constants';
+
 /**
- * 특정 article에 좋아요를 누른 사용자 목록 요청
- * GET /likes?articleId={articleId}
+ * 특정 사용자의 좋아요 목록 조회
+ * GET /bookmark
  */
 
 /**
@@ -14,41 +16,38 @@ import { JWT } from 'next-auth/jwt';
  * POST /likes
  * body {
  *  articleId: string,
- *  isLike: boolean // 좋아요 누를 경우 true, 취소할 경우 false
+ *  isSave: boolean // bookmark 등록시 true, 취소시 false
  * }
- *  */
+ **/
+
 export default withCheckJwt(async (req: NextApiRequest, res: NextApiResponse, token: JWT) => {
-  if (req.method === 'GET') {
-    const articleId = req.query.articleId as string;
-    if (!articleId) return res.status(400).json({ message: 'not valid' });
-
-    try {
-      const requestUrl = `${API_SERVER_URL}/likes?articleId=${articleId}`;
-      const { data, status } = await axios.get(requestUrl);
-      console.log(data);
-      return res.status(status).json(data);
-    } catch (e) {
-      console.log(e);
-      return res.status(500).json({ message: e.message });
-    }
-  } else if (req.method === 'POST') {
-    const { articleId, isLike } = req.body;
+  try {
     const { sub: providerId } = token;
-    if (!articleId || !providerId || isLike === undefined)
-      return res.status(401).json({ message: 'articleId, providerId, isLike가 필요합니다.' });
+    if (!providerId) return res.status(400).json({ message: 'not valid' });
 
-    try {
+    // 프로바이더 좋아요 목록 요청
+    if (req.method === 'GET') {
+      const requestUrl = `${API_SERVER_URL}/likes?providerId=${providerId}`;
+      const { data, status } = await axios.get<LikesResponse>(requestUrl);
+      return res.status(status).json({ infos: data.likes });
+    }
+    // 프로바이더 좋아요 업데이트 요청
+    if (req.method === 'POST') {
+      const { articleId, isSave } = req.body;
+      if (!articleId || typeof isSave !== 'boolean') {
+        return res.status(401).json({ message: 'articleId, providerId, isSave가 필요합니다.' });
+      }
       const requestUrl = `${API_SERVER_URL}/likes`;
       const { data, status } = await axios.post(requestUrl, {
         articleId,
         providerId,
-        isLike,
+        isSave,
       });
-      console.log(data);
       return res.status(status).json(data);
-    } catch (e) {
-      console.log(e.message);
-      return res.status(500).json({ message: e.message });
     }
+  } catch (e) {
+    return res.status(500).json({ message: e.message });
   }
+
+  return res.status(405).end();
 });
