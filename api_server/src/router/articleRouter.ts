176c881e026b7e859a2922cd43fbd12cd73503f8
euntaek <zmn5119@gmail.com>
2021-06-08 21:46:55 +0900

    [UPDATE] 필터 타입 복수형으로 변경, 특정 아티클 조회 분리

--- api_server/src/router/articleRouter.ts
+++ api_server/src/router/articleRouter.ts
@@ -1,31 +1,33 @@
 import express, { Request, Response } from 'express';
 import { CommentsModel } from '../model/Comments';
 
-import articleService from '../service/articleService.new';
+import articleService from '../service/articleService';
 import commentService from '../service/commentService';
 
 const router = express.Router();
 
-type Filter = 'like' | 'history' | 'bookmark';
+type FilterType = 'likes' | 'histories' | 'bookmarks';
+const FILTER = ['likes', 'histories', 'bookmarks'] as const;
 
 // articleId가 query string에 존재하면 특정 아티클 정보만 가져옴, page 무시됨.
 // articleId가 없는 경우 페이징 결과를 반환.
 router.get('/', async (req: Request, res: Response) => {
-  const { articleId, page, providerId, filter } = req.query as {
+  const { page, providerId, filter } = req.query as {
     articleId: string;
     page: string;
     providerId: string;
-    filter: Filter;
+    filter: FilterType;
   };
-
+  if (!page) return res.status(400).json({ message: 'page가 필요합니다' });
+  console.log(req.query);
   // 필터에 따라 아티클 조회
-  if (filter && providerId) {
+  if (FILTER.includes(filter) && providerId) {
     try {
       if (!page) return res.status(400).json({ message: 'page가 필요합니다' });
       const getArticles = {
-        like: () => articleService.findAllByLike(parseInt(page, 10), providerId),
-        history: () => articleService.findAllByHistory(parseInt(page, 10), providerId),
-        bookmark: () => articleService.findAllByBookmark(parseInt(page, 10), providerId),
+        likes: () => articleService.findAllByLike(parseInt(page, 10), providerId),
+        histories: () => articleService.findAllByHistory(parseInt(page, 10), providerId),
+        bookmarks: () => articleService.findAllByBookmark(parseInt(page, 10), providerId),
       };
       const { docs, ...extra } = await getArticles[filter]();
       return res.status(200).json({ data: docs, ...extra });
@@ -35,24 +37,26 @@ router.get('/', async (req: Request, res: Response) => {
     }
   }
 
-  if (articleId) {
-    try {
-      const article = await articleService.findOneById(articleId, providerId);
-      if (!article) return res.status(404).json({ message: '존재하지 않는 article입니다.' });
-      return res.status(200).json({ message: '정상적으로 처리되었습니다.', article });
-    } catch (e) {
-      console.log(e);
-      return res.status(500).json({ message: '서버 에러' });
-    }
-  }
-
-  if (!page) return res.status(400).json({ message: 'page가 필요합니다' });
   try {
     const { docs, ...extra } = await articleService.findAllByPage(parseInt(page), providerId);
     return res.status(200).json({ message: '정상적으로 처리되었습니다.', data: docs, ...extra });
   } catch (e) {
     console.log(e);
     return res.status(500).json({ message: '에러가 발생했습니다.' });
+  }
+});
+
+router.get('/:articleId', async (req, res) => {
+  const { articleId } = req.params as { articleId: string };
+  const { providerId } = req.query as { providerId: string };
+  if (!articleId) return res.status(500).json({ message: '서버 에러' });
+  try {
+    const article = await articleService.findOneById(articleId, providerId);
+    if (!article) return res.status(404).json({ message: '존재하지 않는 article입니다.' });
+    return res.status(200).json({ message: '정상적으로 처리되었습니다.', article });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({ message: '서버 에러' });
   }
 });
 
