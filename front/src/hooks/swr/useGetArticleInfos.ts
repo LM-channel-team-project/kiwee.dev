b7ced3b8e991d46d969ab4b593760aa5147f3fc2
euntaek <zmn5119@gmail.com>
2021-06-08 21:22:40 +0900

    [ADD]  useGetArticleInfos 훅 추가
    사용자의 좋아요, 북마크, 히스토리 목록을 가져옴.
    아티클을 가져오는게 아니라 아티클 아이디를 배열로 가져옴.

---
+++ front/src/hooks/swr/useGetArticleInfos.ts
@@ -0,0 +1,27 @@
+import useRequest, { Config } from './useRequest';
+
+import { ProviderArticleInfo, ProviderArticleInfos } from '@/types/response';
+import { FilterType } from '@/types/apiType';
+
+export function useGetArticlesInfos(filter: FilterType, confing?: Config<ProviderArticleInfos>) {
+  const { data: res, mutate, isValidating } = useRequest<ProviderArticleInfos>(
+    { url: `/${filter}` },
+    confing,
+  );
+  const refresh = (info?: ProviderArticleInfo, shouldRevalidate = false) => {
+    if (!res) throw new Error('Cannot find useGetArticlesInfos');
+    info
+      ? mutate(
+          { ...res, data: { ...res.data, infos: res.data.infos.concat(info) } },
+          shouldRevalidate,
+        )
+      : mutate();
+  };
+
+  return {
+    infos: res?.data.infos || [],
+    refresh,
+    mutate,
+    isValidating,
+  };
+}
