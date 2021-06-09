8cfab1cba3aaaf711a3109e5db44e251b410dff6
97baek <170410_@naver.com>
2021-06-09 22:32:21 +0900

    [UPDATE] index 페이지에 useGetArticles 적용

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,5 +1,6 @@
 // import Keywords from '@/components/Keywords';
 import PostCardLayout from '@/components/PostCardLayout';
+import { useGetArticles } from '@/hooks/swr/useGetArticles';
 import { useGetData } from '@/hooks/useGetData';
 import useInfiniteScroll from '@/hooks/useInfiniteScroll';
 import React, { useEffect, useState } from 'react';
@@ -9,36 +10,22 @@ import React, { useEffect, useState } from 'react';
 //   keyword === 'All' ? articles : articles.filter((article) => article.keywords.includes(keyword));
 
 function Home() {
-  const INITIAL_PAGE_NUMBER = 1;
-  const [currentPage, setCurrentPage] = useState(INITIAL_PAGE_NUMBER);
-
-  /**
-   * articles: 게시글 정보가 담겨있는 배열
-   * pages: 페이지 수 (마지막 페이지)
-   * isLoading: 로딩여부
-   */
-  const { articles, pages, isLoading } = useGetData(`/article?page=${currentPage}`);
+  const { articles, onNextPage, hasNextPage, isValidating, refresh } = useGetArticles();
 
   // 무한 스크롤
   const handleObserver: IntersectionObserverCallback = ([entry]) => {
     if (entry.isIntersecting) {
-      setCurrentPage((prev) => prev + 1);
+      onNextPage();
     }
   };
 
   const [onInfiniteScrollUpdate, onInfiniteScrollDisconnect] = useInfiniteScroll(handleObserver);
 
   useEffect(() => {
-    if (0 < articles.length && articles.length < 21) {
-      onInfiniteScrollUpdate(document.querySelector('footer'));
-    }
-  }, [articles]);
-
-  useEffect(() => {
-    if (currentPage === pages) {
-      onInfiniteScrollDisconnect(document.querySelector('footer'));
-    }
-  }, [currentPage]);
+    const target = document.querySelector('footer');
+    if (articles.length > 1) onInfiniteScrollUpdate(target);
+    if (!hasNextPage) onInfiniteScrollDisconnect(target);
+  }, [articles, hasNextPage]);
 
   // // 필터링
   // const [filter, setFilter] = useState<string>('All');
@@ -51,7 +38,6 @@ function Home() {
 
   return (
     <>
-      {/* <Keywords keywords={keywords} handleFiltering={handleFiltering} /> */}
       <PostCardLayout articles={articles} />
     </>
   );
