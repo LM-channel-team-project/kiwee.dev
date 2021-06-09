814c1b5e6d041b68bfb5833701c783dbbfe9d33a
97baek <170410_@naver.com>
2021-06-09 23:08:45 +0900

    [FIX] 필요없는 코드 정리

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,13 +1,7 @@
-// import Keywords from '@/components/Keywords';
 import PostCardLayout from '@/components/PostCardLayout';
 import { useGetArticles } from '@/hooks/swr/useGetArticles';
-import { useGetData } from '@/hooks/useGetData';
 import useInfiniteScroll from '@/hooks/useInfiniteScroll';
-import React, { useEffect, useState } from 'react';
-
-// // 키워드 api 바뀌면 요청 새로 보내기
-// const filterArticles = (articles: IArticle[], keyword: string) =>
-//   keyword === 'All' ? articles : articles.filter((article) => article.keywords.includes(keyword));
+import React, { useEffect } from 'react';
 
 function Home() {
   const { articles, onNextPage, hasNextPage, isValidating, refresh } = useGetArticles();
@@ -27,20 +21,7 @@ function Home() {
     if (!hasNextPage) onInfiniteScrollDisconnect(target);
   }, [articles, hasNextPage]);
 
-  // // 필터링
-  // const [filter, setFilter] = useState<string>('All');
-  // const handleFiltering = (key: string) => {
-  //   setFilter(key);
-  //   console.log(filter);
-  // };
-
-  // const filteredArticles = useMemo(() => filterArticles(articles, filter), [articles, filter]);
-
-  return (
-    <>
-      <PostCardLayout articles={articles} />
-    </>
-  );
+  return <PostCardLayout articles={articles} isLoading={isValidating} />;
 }
 
 export default Home;
