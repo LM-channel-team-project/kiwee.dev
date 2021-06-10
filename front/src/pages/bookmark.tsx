48465fceeba903e88568e92db391ca976226cfe5
Leo-Xee <jang.xmin@gmail.com>
2021-06-10 22:43:35 +0900

    [UPDATE] swr hook 적용

--- front/src/pages/bookmark.tsx
+++ front/src/pages/bookmark.tsx
@@ -1,44 +1,13 @@
-import React, { useEffect, useState } from "react";
-import { client } from '@/lib/api/client';
+import React from "react";
 import PostCardLayout from "@/components/PostCardLayout";
-import { IArticle } from "@/types/article";
-
-interface Bookmark {
-  articleId: string,
-  _id: string
-}
+import { useGetArticles } from "@/hooks/swr/useGetArticles";
 
 function bookmark() {
-  const [articles, setArticles] = useState<IArticle[]>([]);
+  const { articles, isValidating } = useGetArticles("bookmarks");
   
-  // 1. 북마크된 articleId를 배열의 요소로 변형
-  const getBookmarkedArticles = async (): Promise<string[] | undefined> => {
-    try {
-      const res = await client.get("/bookmarks")
-      const bookmarks = res.data.infos;
-      const articles = bookmarks.map((bookmark: Bookmark) => bookmark.articleId);
-      return articles;
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  
-  // 2. articleId로 게시글 정보를 배열로 변형 
-  useEffect(() => {
-    getBookmarkedArticles()
-    .then(async data => {
-      const promises = data.map(async (id: string): Promise<IArticle> => { const article = await client.get(`/article?articleId=${id}`); return article.data.article;
-      });
-      const articles = await Promise.all(promises);
-      setArticles(articles);
-    }) 
-}, [])
-  
-getBookmarkedArticles();
-
   return (
     <>
-      <PostCardLayout articles={articles} />
+      <PostCardLayout articles={articles} isLoading={isValidating} />
     </>
   )
 }
