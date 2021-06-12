bbb5897a0f4cc01125237e8fd08412d8470db148
euntaek <zmn5119@gmail.com>
2021-06-12 16:42:27 +0900

    [UPDATE] useGetMe 로그인 상태가 아닌 경우 빈객체 반환하도록 수정

--- front/src/hooks/swr/useGetMe.ts
+++ front/src/hooks/swr/useGetMe.ts
@@ -1,14 +1,17 @@
-import useRequest, { Config } from './useRequest';
+import { AxiosRequestConfig } from 'axios';
+import { useSession } from 'next-auth/client';
 
 import { ProviderMeResponse } from '@/types/response';
-import { AxiosRequestConfig } from 'axios';
+import useRequest, { Config } from './useRequest';
 
 const request: AxiosRequestConfig = { url: '/provider/me' };
 
 export const GET_ME_KEY = JSON.stringify(request);
 
 const useGetMe = (confing?: Config<ProviderMeResponse>) => {
-  const { data: res } = useRequest<ProviderMeResponse>(request, confing);
+  const [session] = useSession();
+
+  const { data: res } = session ? useRequest<ProviderMeResponse>(request, confing) : { data: null };
   return {
     provider: res?.data.provider,
   };
