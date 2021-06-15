5c0624f95a8d1e9127ecf1f05ee0f08749580e4a
euntaek <zmn5119@gmail.com>
2021-06-16 01:50:55 +0900

    [ADD] SEO 메타 태그 기본 정적 데이터 추가

---
+++ front/src/lib/static/SEO.ts
@@ -0,0 +1,10 @@
+// default tag
+const SEO_DATA = {
+  name: 'wikee.dev',
+  title: 'wikee.dev | 여러 IT기술 블로그의 최신 게시물을 한 곳에서',
+  description: 'wikee.dev에서는 여러 IT기술 블로그의 최신 게시물을 쉽게 찾아볼 수 있습니다.',
+  url: 'https://wikee.dev',
+  imagePath: '/assets/logo/meta-og-image.png',
+};
+
+export default SEO_DATA;
