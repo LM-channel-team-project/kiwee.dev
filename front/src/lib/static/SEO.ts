3a3c2b614dd77a826830a541db88534b6ba4003a
euntaek <zmn5119@gmail.com>
2021-06-16 17:04:49 +0900

    [FIX] 오타 수정

--- front/src/lib/static/SEO.ts
+++ front/src/lib/static/SEO.ts
@@ -1,9 +1,9 @@
 // default tag
 const SEO_DATA = {
-  name: 'wikee.dev',
-  title: 'wikee.dev | 여러 IT기술 블로그의 최신 게시물을 한 곳에서',
-  description: 'wikee.dev에서는 여러 IT기술 블로그의 최신 게시물을 쉽게 찾아볼 수 있습니다.',
-  url: 'https://wikee.dev',
+  name: 'kiwee.dev',
+  title: 'kiwee.dev | 여러 IT기술 블로그의 최신 게시물을 한 곳에서',
+  description: 'kiwee.dev에서는 여러 IT기술 블로그의 최신 게시물을 쉽게 찾아볼 수 있습니다.',
+  url: 'https://kiwee.dev',
   imagePath: '/assets/logo/meta-og-image.png',
 };
 
