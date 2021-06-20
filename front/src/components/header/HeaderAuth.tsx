39681396698d6d71a0103da2435be2de10574b61
euntaek <zmn5119@gmail.com>
2021-06-20 17:25:22 +0900

    [UPDATE] 프로필 메뉴 드랍다운 추가

--- front/src/components/header/HeaderAuth.tsx
+++ front/src/components/header/HeaderAuth.tsx
@@ -1,36 +1,43 @@
-import React from 'react';
-import Link from 'next/link';
+import { useState } from 'react';
 import Image from 'next/image';
-import { Session } from 'next-auth';
 import styled from 'styled-components';
+import { Session } from 'next-auth';
+
+import HeaderMenu from './HeaderMenu';
 import TextButton from '@/components/Common/Button/Text';
 
-function HeaderAuth({
-  session,
-  toggleModal,
-}: {
+interface HeaderAuthProps {
   session: Session | null;
   toggleModal: () => void;
-}) {
+}
+
+function HeaderAuth({ session, toggleModal }: HeaderAuthProps) {
+  const [openMenu, setOpenMenu] = useState(false);
+  const onOpenMenu = () => {
+    setOpenMenu((open) => !open);
+  };
+  const onClose = () => {
+    setOpenMenu(false);
+  };
+
   return (
     <HeaderAuthBlock>
       {!session ? (
         <TextButton label="Login" styleType="primary" size="large" onClick={toggleModal} />
       ) : (
-        <Link href="/profile">
-          <a>
-            <Image
-              loader={imageLoader}
-              src={session.user?.image || 'null'}
-              width="40"
-              height="40"
-              alt="profile image"
-              layout="intrinsic"
-              className="profile-image"
-            />
-          </a>
-        </Link>
+        <button className="profile-button" onClick={onOpenMenu}>
+          <Image
+            loader={imageLoader}
+            src={session.user?.image || 'null'}
+            width="40"
+            height="40"
+            alt="profile image"
+            layout="fixed"
+            className="profile-image"
+          />
+        </button>
       )}
+      {openMenu && <HeaderMenu onClose={onClose} />}
     </HeaderAuthBlock>
   );
 }
@@ -40,9 +47,22 @@ function imageLoader({ src }: { src: string }) {
 }
 
 const HeaderAuthBlock = styled.div`
+  position: relative;
   display: flex;
   justify-content: center;
   align-items: center;
+  .profile-button {
+    width: 4rem;
+    height: 4rem;
+    padding: 0;
+    border: none;
+    border-radius: 0.4rem;
+    cursor: pointer;
+  }
+
+  img {
+    border-radius: 0.4rem;
+  }
 `;
 
 export default HeaderAuth;
